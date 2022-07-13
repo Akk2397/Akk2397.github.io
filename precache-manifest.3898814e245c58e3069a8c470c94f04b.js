ba49da622fb212d736c228503ba7845eeaf43c47
Akshay Kumar Kushwaha <akk09231997@gmail.com>
2022-07-13 22:45:44 +0530

    Updates

---
+++ precache-manifest.3898814e245c58e3069a8c470c94f04b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2a529649ee27037300e",
+    "url": "/static/css/main.74796357.chunk.css"
+  },
+  {
+    "revision": "f2a529649ee27037300e",
+    "url": "/static/js/main.a3f87210.chunk.js"
+  },
+  {
+    "revision": "e4686e9a92b21e222891",
+    "url": "/static/js/runtime~main.99c07c0c.js"
+  },
+  {
+    "revision": "d46bf0c1f142fd38ad3e",
+    "url": "/static/css/2.b6485d37.chunk.css"
+  },
+  {
+    "revision": "d46bf0c1f142fd38ad3e",
+    "url": "/static/js/2.adf63c87.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f0e381d9620d05ebc8e255e2087516f8",
+    "url": "/index.html"
+  }
+];
